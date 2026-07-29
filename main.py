dcdbf56f9eed9a9b183b911c34e1d522048f2969
Efrem273 <efremnegasi273@gmail.com>
2026-07-29 22:23:31 +0300

    Test CORS

--- main.py
+++ main.py
@@ -69,10 +69,11 @@ origins = [
     "http://localhost:5680",
     "https://editingpro.netlify.app",
 ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
